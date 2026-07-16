d37a7f0c60598db0820705ccebcb92ecac00aa9b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 16:34:09 +0300

    my turn - 0bcf3f429e482e1e9d3a20e46c73b910e2718d4c

--- src/components/Student/Student.tsx
+++ src/components/Student/Student.tsx
@@ -1,7 +1,16 @@
-export default function Student(props){
-    console.log(props);
-    
-    return (
-        <p>student</p>
-    )
+import type { Student } from "../../types";
+
+interface StudentProps {
+  student: Student;
+}
+
+export default function StudentItem({ student }: StudentProps) {
+  return (
+    <div>
+      <img src={student.avatar} alt={student.name} />
+      <h2>{student.name}</h2>
+      <p>Age: {student.age}</p>
+      <p>Course: {student.course}</p>
+    </div>
+  );
 }
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface Student {
+  id: number;
+  name: string;
+  age: number;
+  course: string;
+  isOnline: boolean;
+  avatar: string;
+}
